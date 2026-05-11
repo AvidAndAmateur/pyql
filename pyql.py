487031cd128404dbf4e7481e8d0943059ea4d9e7
Ben <52603515+AvidAndAmateur@users.noreply.github.com>
2026-05-11 19:36:52 +0100

    added all main functions
    now able to delete values, delete tables, create tables, executing custom sql commands. When executing commands that delete data (using prebuilt command or use SQLCMD function, it will ask the user whether they are sure they want to proceed). The program now checks to see if the database file does actually exist

--- pyql.py
+++ pyql.py
@@ -2,17 +2,24 @@ import sqlite3
 import sys
 import os
 import time
+from pathlib import Path
 def clear():
         os.system('cls' if os.name == 'nt' else 'clear')
 try:
     def setup():
+        clear()
         global db
         global SQLDB
         global cursor
-        db = input("enter name or path of database: ")
-        SQLDB = sqlite3.connect(f'{db}')
-        cursor = SQLDB.cursor()
-        main()
+        db = Path(input("enter name or path of database: "))
+        if db.exists():
+            SQLDB = sqlite3.connect(f'{db}')
+            cursor = SQLDB.cursor()
+            main()
+        else:
+            print("database does not exist, returning to setup")
+            time.sleep(3)
+            setup()
     def main():
         clear()
         print(f'connected to {db}')
@@ -24,6 +31,7 @@ try:
              time.sleep(5)
              main()
         if userinp == "updV":
+             clear()
              tblname = input("enter table name: ")
              upcolname = input("column name: ")
              valname = input("new value: ")
@@ -33,6 +41,7 @@ try:
              SQLDB.commit()
              main()
         if userinp == "insV":
+             clear()
              tablname = input("enter table name: ")
              incolname = input("enter column name: ")
              addval = input("value to add: ")
@@ -40,6 +49,62 @@ try:
              cursor.execute(query)
              SQLDB.commit()
              main()
+        if userinp == "del":
+             clear()
+             tablname = input("enter table name: ")
+             valname = input("enter value name: ")
+             operator = input("operator: ")
+             valwhere = input(f"where value {operator}:  ")
+             query = f"DELETE FROM {tablname} WHERE {valname} {operator} '{valwhere}'"
+             cursor.execute(query)
+             SQLDB.commit()
+             main()
+        if userinp == "crtT":
+             tablname = input("enter table name: ")
+             column = input("column name: ")
+             query = f"CREATE TABLE {tablname} ({column} TEXT)"
+             cursor.execute(query)
+             SQLDB.commit()
+             main()
+        if userinp == "delT":
+             clear()
+             tablname = input("enter table name: ")
+             print("THIS CHANGE IS PERMANENT")
+             areyousure = input("please type yes if you're sure: ") 
+             if areyousure.lower() == "yes":
+                 query = f"DROP TABLE {tablname}"
+                 cursor.execute(query)
+                 SQLDB.commit()
+                 main()
+             else:
+              print("cancelling operation")
+              time.sleep(3)
+              main()
+        if userinp.upper() == "SQLCMD":
+            clear()
+            cmd = input("Enter sql cmd: ")
+            fetching = input("fetching vals: ")
+            if fetching.lower() == "yes":
+                cursor.execute(cmd)
+                print(cursor.fetchall())
+                time.sleep(5)
+                main()
+            elif "DROP" or "DELETE" in cmd:
+                print("are you sure you want to execute this? \n it contains permanent value modifications")
+                sure = input("yes/no: ")
+                if sure.lower() == "yes":
+                    cursor.execute(cmd)
+                    SQLDB.commit()
+                    main()
+                else:
+                    print("operation cancelled, returning to main menu")
+                    time.sleep(3)
+                    main()
+            else:
+                cursor.execute(cmd)
+                SQLDB.commit()
+                main()
+                
     args = sys.argv[1:]
     if  len(args)>0 and args[0].lower() == "-h":
         print("options: \n list tables (lsT), update values(updV) \n insert values (insV), delete values (delV) \n list all value \n find value")
